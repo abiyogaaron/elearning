c4838ef75bd89c7516408c881618e7774c6952db
aaron abiyoga <aaron.sulistio@shopee.com>
2021-02-11 19:04:20 +0700

    update .yml

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit
           {' '}
-          <code>src/App.tsx</code>
+          <code>PIPELINE TESTING</code>
           {' '}
           and save to reload.
         </p>
